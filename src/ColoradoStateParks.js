fa7acc1015a8cc02bdcdf6c62413099bf0e11c20
Gian Carlo De Castro <Giancld92@gmail.com>
2022-09-20 11:53:41 -0500

    commit tree 827798823a5f2f5d5d2acb3e2b9b3e8bb5ad830f

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,15 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <MesaVerde/>
+    </div>
+  )
 }
+console.log(trees)
+console.log(wildlife)
+
+export default ColoradoStateParks;
